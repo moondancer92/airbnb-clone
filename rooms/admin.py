2c5ab681fd41cc62eafb64e7078beda549e4e479
moondancer92 <70056397+moondancer92@users.noreply.github.com>
2021-01-27 19:48:54 +0900

    created_room
    added room models
    
    -RoomType
    -Amenity
    -Facility
    -HouseRule
    -Room
    -Room_Photo

--- rooms/admin.py
+++ rooms/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+@admin.register(models.RoomType, models.Facility, models.Amenity, models.HouseRule)
+class ItemAdmin(admin.ModelAdmin):
+
+    """Item Admin Definition"""
+
+    pass
+
+
+@admin.register(models.Room)
+class RoomAdmin(admin.ModelAdmin):
+
+    """Room Admin Definiton """
+
+    pass
+
+
+@admin.register(models.Photo)
+class PhotoAdimin(admin.ModelAdmin):
+    """Photo Admin Definition """
+
+    pass
